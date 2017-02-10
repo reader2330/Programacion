1c5520f46390248fbb8ce99d9b9fc05ce7f6fde0
reader2330 <read2330@gmail.com>
2017-02-09 23:49:28 -0600

    Practica 3

--- practicas/P03/P3_2.java
+++ practicas/P03/P3_2.java
@@ -2,14 +2,21 @@
 public class P3_2{
 	public static void main(String[] args){
      int a=1000;
+     int b=7;
     long ti , tf, t;
     ti=System.currentTimeMillis();
-      if(a>0 && a<1001){
+      if((a>0 && a<1001) && (b>0 && a<1001)){
 
      if(a%2==0) 
      System.out.println("input is " +a+ ",then output should be:'Even'");
      else
      System.out.println("input is " +a+",then output should be:'Odd'");
+     if(b%2==0)
+     	System.out.println("input is " +b+ ",then output should be:'Even'");
+     else
+     	System.out.println("input is " +b+",then output should be:'Odd'");
+     
+     
 } else 
       System.out.println("El numero no es valido");
 
